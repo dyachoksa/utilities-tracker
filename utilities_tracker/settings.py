f0664777ee6a8820f8a12afbbb9d7d1b93ba7436
Sergey Dyachok <sergey@sdyachok.com.ua>
2024-03-04 23:20:40 +0200

    design: update basic styling

--- utilities_tracker/settings.py
+++ utilities_tracker/settings.py
@@ -269,9 +269,16 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[{asctime}] [{levelname:>8}] {name}: {message}",
+            "style": "{",
+        }
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
     },
     "root": {
